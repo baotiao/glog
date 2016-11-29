ecdbd7cda69e1ff304ac02f7f277715a162e1474
CatKang <beijingwangkang@hotmail.com>
2016-11-29 14:18:06 +0800

    Split log info with different severity into different file

--- src/logging.cc
+++ src/logging.cc
@@ -728,8 +728,9 @@ inline void LogDestination::LogToAllLogfiles(LogSeverity severity,
   if ( FLAGS_logtostderr ) {           // global flag: never log to file
     ColoredWriteToStderr(severity, message, len);
   } else {
-    for (int i = severity; i >= 0; --i)
-      LogDestination::MaybeLogToLogfile(i, timestamp, message, len);
+    //for (int i = severity; i >= 0; --i)
+    //  LogDestination::MaybeLogToLogfile(i, timestamp, message, len);
+    LogDestination::MaybeLogToLogfile(severity, timestamp, message, len);
   }
 }
 
